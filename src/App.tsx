fe83739ec1b6f19afccbdeb871c41533fe58aa1a
pedro <pedrohenriquemartins@live.com>
2022-04-05 23:05:17 -0300

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
-      <h1 className='title'>Hello World</h1>
-      <GlobalStyle />  
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
