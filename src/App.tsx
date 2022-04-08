b72a57ff7c4f8b0a81c5eee482711c992f671b38
pedro <pedrohenriquemartins@live.com>
2022-04-07 23:58:51 -0300

    add Summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import { Summary } from "./components/Summary";
 
 export function App() {
   return (
     <>
       <Header />
+      <Summary />
       <GlobalStyle />
     </>
   );
